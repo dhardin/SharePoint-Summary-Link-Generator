1c5b2fdf53fa60f6630fad7276501ec5d0c09a44
Dustin Hardin <dhardin07@gmail.com>
2015-07-30 16:24:48 -0400

    modal window fix

--- js/config.js
+++ js/config.js
@@ -1,16 +1,18 @@
 var app = app || {};
+app.testing = true;
 
 app.config = {
-    url: '',
-    guid: ''
+    url: 'https://members.lcmp.csd.disa.mil/sites/jpojltv/ops/',
+    guid: '29A8DF58-8771-4787-9C70-6D4189A8CDDD'
 };
 
 
 app.property_map = {
-    ows_department: 'department',
     ows_description: 'description',
+    ows_title : 'title',
     ows_id: 'id',
-    ows_modified: 'modified',
-    ows_created: 'created',
-    ows_date: 'date'
+    ows_iconclass: 'iconClass',
+    ows_imgurl: 'imgUrl',
+    ows_border: 'border',
+    ows_backgroundcolor: 'backgroundColor'
 };
